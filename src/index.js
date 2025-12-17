6d17731b53da4d005992848c772d0bae6e71f891
Nikhil2690 <nikhiladwani3@gmail.com>
2025-12-17 19:55:44 +0530

    added utils for api responses, error handling and  async operations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 //import mongoose from "mongoose"
 //import { DB_NAME } from "./constants"
 
@@ -8,7 +9,15 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=> {
+    console.log("MONGO DB connection failed !!! ", err)
+})
 
 // (async ()=> {
 //     try{
